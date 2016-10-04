fcaced34bf2d1de53fd400b9beb945ebd192d8c0
Néstor Palafox <nestor.garcia@tcpip.tech>
2016-10-04 11:29:43 -0500

    IIFE

--- test/index.js
+++ test/index.js
@@ -4,10 +4,28 @@ var lib = require('../lib/index');
 
 describe('concat function', function () {
 
+  it('returns empty string when the function is called without parameters', function (done) {
+    var concat = lib.concat();
+    concat.should.equal("");
+    done();
+  });
+
+  it('returns empty string when the function is called with an empty array', function (done) {
+    var concat = lib.concat([]);
+    concat.should.equal("");
+    done();
+  });
+
+  it('returns empty string when the function is called with null', function (done) {
+    var concat = lib.concat(null);
+    concat.should.equal("");
+    done();
+  });
+
+
   it('concatenates not null parameters into a string', function (done) {
     var concat = lib.concat("hello", null, "world!");
     concat.should.equal("hello world!");
-    console.log(concat);
     done();
   });
 
